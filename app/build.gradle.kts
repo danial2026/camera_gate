d85cb37b0c68fb2f15c49459e25c4484123b9807
Danial M. <danial@danials.org>
2026-08-13 18:25:58 +0330

    chore(app): bump version to 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.danials.cameragate"
         minSdk = 16
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
     }
 
     compileOptions {
